535483b99969443c80f0c310ceebcdd6694047ab
Ernest Addae <earnestaddae@gmail.com>
2021-07-07 08:46:21 +0800

    Allow dynamic selection of pay_type with React

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -80,5 +80,17 @@ class OrdersController < ApplicationController
       params.require(:order).permit(:name, :address, :email, :pay_type)
     end
 
+    # whitelist pay_type params for order
+    def pay_type_params
+      if order_params[:pay_type] == "Credit card"
+        params.require(:order).permit(:credit_card_number, :expiration_date)
+      elsif order_params[:pay_type] == "Check"
+        params.require(:order).permit(:routing_number, :account_number)
+      elsif order_params[:pay_type] == "Purchase order"
+        params.require(:order).permit(:po_number)
+      else
+        {}
+      end
+    end
 
 end
